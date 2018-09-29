77c31cf91fea3787195075445712d527c6e40510
omijn <abhijit.kashyap07@gmail.com>
2018-09-29 10:13:23 -0700

    get stack exchange tags using API or reading from file

--- scrape.py
+++ scrape.py
@@ -2,15 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import argparse
 import json
-
+import time
 
 class SEApiClient:
     base_url = "https://api.stackexchange.com/2.2/"
     sites_endpoint = "sites"
     tags_endpoint = "tags"
 
-    def sites_api(self):
-        r = requests.get(self.base_url + self.sites_endpoint, params={"pagesize": 300})
+    @classmethod
+    def sites_api(cls):
+        r = requests.get(cls.base_url + cls.sites_endpoint, params={"pagesize": 300})
         response = r.json()
         formatted_response = {"items": {}}
         for site_info in response["items"]:
@@ -21,6 +22,19 @@ class SEApiClient:
                 "logo_url": site_info["logo_url"],
                 "site_type": site_info["site_type"]
             }
+        return formatted_response
+
+    @classmethod
+    def tags_api(cls, tagcount, site_source):
+        formatted_response = {"items": {}}
+        print("Currently getting tags for: ")
+        for site in site_source["items"].keys():
+            print(site)
+            r = requests.get(cls.base_url + cls.tags_endpoint,
+                             params={"site": site, "pagesize": tagcount, "order": "desc", "sort": "popular"})
+            response = r.json()
+            formatted_response["items"][site] = list(map(lambda item: item["name"], response["items"]))
+            time.sleep(40)
         return formatted_response
 
 
@@ -40,12 +54,20 @@ def main():
     args = parser.parse_args()
 
     if args.sites_api:
-        site_source = SEApiClient().sites_api()
+        site_source = SEApiClient.sites_api()
         with open(args.sites_outfile, "w") as f:
             json.dump(site_source, f)
     else:
         with open(args.sites_infile, "r") as f:
             site_source = json.load(f)
+
+    if args.tags_api:
+        tag_source = SEApiClient.tags_api(args.tagcount, site_source)
+        with open(args.tags_outfile, "w") as f:
+            json.dump(tag_source, f)
+    else:
+        with open(args.tags_infile, "r") as f:
+            tag_source = json.load(f)
 
 
 if __name__ == '__main__':
